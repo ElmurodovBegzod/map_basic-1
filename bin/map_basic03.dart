5f2b0b27fcd046849564c69fa5e9f0038f10d20a
Begzod <ebegzod477@gmail.com>
2022-08-23 14:24:31 +0500

    code03

--- bin/map_basic03.dart
+++ bin/map_basic03.dart
@@ -7,9 +7,12 @@ Return true if the length of the map is greater than zero, otherwise false.
 */
 
 bool func(Map dct) {
-  return;
+  if (dct.length > 0) {
+    return true;
+  } else
+    return false;
 }
 
 void main() {
-  print(func());
+  print(func({1: 'B', 2: 'C', 3: 'D'}));
 }
